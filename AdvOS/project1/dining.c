fe8de199cff0141aa6bbdd5a1eef7aa9a9b93bc6
cricketgeek <jonesmc123@gmail.com>
2014-08-27 07:17:57 -0700

    cave man debugging

--- AdvOS/project1/dining.c
+++ AdvOS/project1/dining.c
@@ -84,15 +84,21 @@ void pickup_chopsticks(int phil_id){
 	}
     pthread_mutex_unlock(&chopstick_mutex[phil_id]);
 	
+	printf("philo %d condition met, picking up chopsticks\n", phil_id );
     pickup_one_chopstick(phil_to_chopstick(phil_id, left), phil_id);
     pickup_one_chopstick(phil_to_chopstick(phil_id, right), phil_id);
 }
 
 void putdown_chopsticks(int phil_id){
   	/*Use putdown_chopstick to put down the chopsticks*/
+  	printf("philo %d attempting to put down chopsticks and signal neighbors %d and %d\n",phil_id, 
+  		left_of_phil(phil_id),right_of_phil(phil_id));
+
   	pthread_mutex_lock(&chopstick_mutex[phil_id]);
   	philo_states[phil_id] = THINKING;
   	pthread_mutex_lock(&chopstick_mutex[phil_id]);
+
+  	printf("philo %d now thinking...\n", phil_id);
 
 	putdown_one_chopstick(phil_to_chopstick(phil_id, left),phil_id);
 	putdown_one_chopstick(phil_to_chopstick(phil_id, right),phil_id);
