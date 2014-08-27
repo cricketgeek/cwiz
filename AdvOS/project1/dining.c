52630a005a44484e0c29b3f0b07782e7966290b4
cricketgeek <jonesmc123@gmail.com>
2014-08-26 18:53:22 -0700

    fixing for linux

--- AdvOS/project1/dining.c
+++ AdvOS/project1/dining.c
@@ -7,10 +7,10 @@
 #include <time.h>
 #include <sys/time.h>
 
-#ifdef __MACH__
-#include <mach/clock.h>
-#include <mach/mach.h>
-#endif
+// #ifdef __MACH__
+// #include <mach/clock.h>
+// #include <mach/mach.h>
+// #endif
 
 typedef enum  {left, right} direction_t;
 struct timespec ts;
@@ -29,23 +29,30 @@ int chopstick_to_phil(int stick_id, direction_t d){
 void pickup_one_chopstick(int stick_id, int phil_id){
 
 	int               rc;
-  	struct timeval    tp;
+    struct timespec ts;
+    struct timeval now;
 
-#ifdef __MACH__ // OS X does not have clock_gettime, use clock_get_time
-	clock_serv_t cclock;
-	mach_timespec_t mts;
-	host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
-	clock_get_time(cclock, &mts);
-	mach_port_deallocate(mach_task_self(), cclock);
-	ts.tv_sec = mts.tv_sec;
-	ts.tv_nsec = mts.tv_nsec;
-#else
-	clock_gettime(CLOCK_REALTIME, &ts);
-	/* Convert from timeval to timespec */
-	ts.tv_sec  = tp.tv_sec;
-	ts.tv_nsec = tp.tv_usec * 1000;
-	rc =  gettimeofday(&tp, NULL);
-#endif
+// #ifdef __MACH__ // OS X does not have clock_gettime, use clock_get_time
+// 	clock_serv_t cclock;
+// 	mach_timespec_t mts;
+// 	host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
+// 	clock_get_time(cclock, &mts);
+// 	mach_port_deallocate(mach_task_self(), cclock);
+// 	ts.tv_sec = mts.tv_sec;
+// 	ts.tv_nsec = mts.tv_nsec;
+// #else
+// 	/*clock_gettime(CLOCK_REALTIME, &ts); */
+// 	/* Convert from timeval to timespec */
+// 	ts.tv_sec  = tp.tv_sec;
+// 	ts.tv_nsec = tp.tv_usec * 1000;
+// 	rc =  gettimeofday(&tp, NULL);
+// #endif
+    gettimeofday(&now, NULL);
+    ts.tv_sec = now.tv_sec;
+    ts.tv_nsec = now.tv_usec * 1000; /* usec to nsec */
+    /* add the offset to get timeout value */
+  	double randTime = rand() / (RAND_MAX + 1.0);    
+    ts.tv_sec += randTime * 10;
 
   	double randTime = rand() / (RAND_MAX + 1.0);
   	printf("randTime is %f\n", randTime);
